382fc455597866044b5c8d6f52d8d027ad198054
Ignacio Rodríguez Varela <nacho.rv2304@gmail.com>
2022-08-03 18:11:07 -0600

    Se realiza una optimizacion en el codigo js que da un efecto a las categorias.

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {titleHover, titleLeave} from '../js/category';
 import { imgCategory } from '../js/imgCategory';
 
 export const Category = () => {
@@ -8,39 +9,39 @@ export const Category = () => {
                 <img src={imgCategory.elec} alt="category-electronics" className="category-img" />
                 <img src={imgCategory.elec2} alt="category-img2" id="imgElec"
                     className="category-hidden" />
-                <a href="#" className="title-category" id="titleElec">Electrónica</a>
+                <a href="#" className="title-category" onMouseOver={e => titleHover('imgElec')} onMouseLeave={e => titleLeave('imgElec')}>Electrónica</a>
             </section>
 
             <section className="category">
                 <img src={imgCategory.woman} alt="category-woman" className="category-img" />
                 <img src={imgCategory.woman2} alt="category-woman" className="category-hidden" id="imgWoman" />
-                <a href="#" className="title-category" id="titleWoman">Mujer</a>
+                <a href="#" className="title-category" onMouseOver={e => titleHover('imgWoman')} onMouseLeave={e => titleLeave('imgWoman')}>Mujer</a>
             </section>
 
             <section className="category">
                 <img src={imgCategory.care} alt="category-care" className="category-img" />
                 <img src={imgCategory.care2} alt="category-care" className="category-hidden" id="imgCare" />
-                <a href="#" className="title-category" id="titleCare">Salud y belleza</a>
+                <a href="#" className="title-category" onMouseOver={e => titleHover('imgCare')} onMouseLeave={e => titleLeave('imgCare')}>Salud y belleza</a>
             </section>
 
             <section className="category-home category">
                 <img src={imgCategory.home} alt="category-home" className="category-img" />
                 <img src={imgCategory.home2} alt="category-home" className="category-hidden" id="imgHome" />
-                <a href="#" className="title-category" id="titleHome">Hogar</a>
+                <a href="#" className="title-category" onMouseOver={e => titleHover('imgHome')} onMouseLeave={e => titleLeave('imgHome')}>Hogar</a>
             </section>
 
             <section className="category">
                 <img src={imgCategory.man} alt="category-man" className="category-img" />
                 <img src={imgCategory.man2} alt="category-man" className="category-hidden"
                     id="imgMan" />
-                <a href="#" className="title-category" id="titleMan">Hombre</a>
+                <a href="#" className="title-category" onMouseOver={e => titleHover('imgMan')} onMouseLeave={e => titleLeave('imgMan')}>Hombre</a>
             </section>
 
             <section className="category">
                 <img src={imgCategory.kids} alt="category-kids" className="category-img" />
                 <img src={imgCategory.kids2} alt="category-kids" className="category-hidden"
                     id="imgKids" />
-                <a href="#" className="title-category" id="titleKids">Infantil</a>
+                <a href="#" className="title-category" onMouseOver={e => titleHover('imgKids')} onMouseLeave={e => titleLeave('imgKids')}>Infantil</a>
             </section>
         </div>
     )
--- frontend/src/js/category.js
+++ frontend/src/js/category.js
@@ -1,89 +1,11 @@
-//Código que permite que al hacer hover en los titulos de las categorias, cambia el fondo de color de elemento contenedor.
+//Código que permite cambiar la imagen de fondo, utilizando las funciones hover y leave.
 
-//Sección de la categoria de electronica.
-const imgElec = document.getElementById("imgElec");
-const titleElec = document.getElementById("titleElec");
+export const titleHover = (idElement) => {
+    const img = document.getElementById(idElement);
+    img.classList.replace('category-hidden', 'category-img');
+};
 
-titleElec.addEventListener("mouseover", () => {
-    imgElec.classList.add("category-img");
-    imgElec.classList.remove("category-hidden");
-});
-
-titleElec.addEventListener("mouseleave", () => {
-    imgElec.classList.add("category-hidden");
-    imgElec.classList.remove("category-img");
-});
-
-
-//Sección de la categoria de mujer.
-const imgWoman = document.getElementById("imgWoman");
-const titleWoman = document.getElementById("titleWoman");
-
-titleWoman.addEventListener("mouseover", () => {
-    imgWoman.classList.add("category-img");
-    imgWoman.classList.remove("category-hidden");
-});
-
-titleWoman.addEventListener("mouseleave", () => {
-    imgWoman.classList.add("category-hidden");
-    imgWoman.classList.remove("category-img");
-});
-
-//Sección de la categoria de belleza.
-const imgCare = document.getElementById("imgCare");
-const titleCare = document.getElementById("titleCare");
-
-titleCare.addEventListener("mouseover", () => {
-    imgCare.classList.add("category-img");
-    imgCare.classList.remove("category-hidden");
-});
-
-titleCare.addEventListener("mouseleave", () => {
-    imgCare.classList.add("category-hidden");
-    imgCare.classList.remove("category-img");
-});
-
-
-//Sección de la categoria de hogar.
-const imgHome = document.getElementById("imgHome");
-const titleHome = document.getElementById("titleHome");
-
-titleHome.addEventListener("mouseover", () => {
-    imgHome.classList.add("category-img");
-    imgHome.classList.remove("category-hidden");
-});
-
-titleHome.addEventListener("mouseleave", () => {
-    imgHome.classList.add("category-hidden");
-    imgHome.classList.remove("category-img");
-});
-
-
-//Sección de la categoria de hombre.
-const imgMan = document.getElementById("imgMan");
-const titleMan = document.getElementById("titleMan");
-
-titleMan.addEventListener("mouseover", () => {
-    imgMan.classList.add("category-img");
-    imgMan.classList.remove("category-hidden");
-});
-
-titleMan.addEventListener("mouseleave", () => {
-    imgMan.classList.add("category-hidden");
-    imgMan.classList.remove("category-img");
-});
-
-
-//Sección de la categoria de infantil.
-const imgKids = document.getElementById("imgKids");
-const titleKids = document.getElementById("titleKids");
-
-titleKids.addEventListener("mouseover", () => {
-    imgKids.classList.add("category-img");
-    imgKids.classList.remove("category-hidden");
-});
-
-titleKids.addEventListener("mouseleave", () => {
-    imgKids.classList.add("category-hidden");
-    imgKids.classList.remove("category-img");
-});
+export const titleLeave = (idElement) => {
+    const img = document.getElementById(idElement);
+    img.classList.replace('category-img', 'category-hidden');
+};
