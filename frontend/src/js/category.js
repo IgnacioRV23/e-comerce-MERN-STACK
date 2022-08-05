131412faa9089dcb430a47507beab7fc49b188a7
Ignacio Rodríguez Varela <nacho.rv2304@gmail.com>
2022-08-04 18:50:21 -0600

    Se realiza el codigo que permite que la pagina principal sea responsive.

--- frontend/src/js/category.js
+++ frontend/src/js/category.js
@@ -1,11 +1,16 @@
 //Código que permite cambiar la imagen de fondo, utilizando las funciones hover y leave.
 
 export const titleHover = (idElement) => {
-    const img = document.getElementById(idElement);
-    img.classList.replace('category-hidden', 'category-img');
+    if (window.screen.width >= 1024) {
+        const img = document.getElementById(idElement);
+        img.classList.replace('category-hidden', 'category-img');
+    }
 };
 
 export const titleLeave = (idElement) => {
-    const img = document.getElementById(idElement);
-    img.classList.replace('category-img', 'category-hidden');
+
+    if (window.screen.width >= 1024) {
+        const img = document.getElementById(idElement);
+        img.classList.replace('category-img', 'category-hidden');
+    }
 };
