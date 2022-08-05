f62d79f95174e56d27d950332ee68de0b3112bf1
Ignacio Rodríguez Varela <nacho.rv2304@gmail.com>
2022-08-04 19:15:06 -0600

    Se realizan modificaciones en los estilos del footer

--- frontend/src/components/Flash.js
+++ frontend/src/components/Flash.js
@@ -56,66 +56,6 @@ export const Flash = () => {
                         <button className="btn btn-red">Comprar</button>
                     </div>
                 </article>
-
-                <article className="card">
-                    <div className="container-card-img">
-                        <div className="tag-flash">
-                            <img src={bolt} alt="icon-bolt" />
-                            <p className="flash">Flash</p>
-                        </div>
-                        <img src={bagWoman} alt="bag-woman" className="product-img" />
-                    </div>
-                    <div className="card-detail">
-                        <p className="product-name">Nombre del producto</p>
-                        <p className="product-detail">Detalle</p>
-                        <p className="price-off">$16</p>
-                        <p className="price-red">$10</p>
-                    </div>
-                    <div className="card-container-btn">
-                        <button className="btn btn-black">Agregar al carrito</button>
-                        <button className="btn btn-red">Comprar</button>
-                    </div>
-                </article>
-
-                <article className="card">
-                    <div className="container-card-img">
-                        <div className="tag-flash">
-                            <img src={bolt} alt="icon-bolt" />
-                            <p className="flash">Flash</p>
-                        </div>
-                        <img src={bagWoman} alt="bag-woman" className="product-img" />
-                    </div>
-                    <div className="card-detail">
-                        <p className="product-name">Nombre del producto</p>
-                        <p className="product-detail">Detalle</p>
-                        <p className="price-off">$16</p>
-                        <p className="price-red">$10</p>
-                    </div>
-                    <div className="card-container-btn">
-                        <button className="btn btn-black">Agregar al carrito</button>
-                        <button className="btn btn-red">Comprar</button>
-                    </div>
-                </article>
-
-                <article className="card">
-                    <div className="container-card-img">
-                        <div className="tag-flash">
-                            <img src={bolt} alt="icon-bolt" />
-                            <p className="flash">Flash</p>
-                        </div>
-                        <img src={bagWoman} alt="bag-woman" className="product-img" />
-                    </div>
-                    <div className="card-detail">
-                        <p className="product-name">Nombre del producto</p>
-                        <p className="product-detail">Detalle</p>
-                        <p className="price-off">$16</p>
-                        <p className="price-red">$10</p>
-                    </div>
-                    <div className="card-container-btn">
-                        <button className="btn btn-black">Agregar al carrito</button>
-                        <button className="btn btn-red">Comprar</button>
-                    </div>
-                </article>
             </div>
         </div>
     )
